b30934b9420d3f180a4e8d2592f0a8754fbf32b5
Zulfa Rajabu <zulfa.rajab@vcontractor.co.tz>
2023-03-30 02:07:48 +0300

    changes

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -8,9 +8,9 @@ function Map() {
             <div className='flex flex-col justify-center  my-5' >
                 <h1 className='text-4xl font-giz text-red-700 text-start mx-10 md:mx-20'>GIZ Tanzania & EAC Map</h1>
                 <div className='flex justify-center mx-10 mt-10'>
-                    <img src={MapImage} alt="my profile" className="w-10/12 mt-4 md:w-2/3 object-cover md:ml-20 " />
+                    <img src={MapImage} alt="my profile" className="w-10/12 mt-4 md:w-2/3 ml-5 object-cover md:ml-20 " />
                     <div>
-                        <img src={MapIcon} alt="map icon" className="w-full object-contain" />
+                        <img src={MapIcon} alt="map icon" className="w-full" />
                     </div>
 
                 </div>
